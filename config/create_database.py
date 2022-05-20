6f83c64a448f3319f22c2733ed7f28f09f1bcebd
Aramis Sarmiento Díaz <aramiissd@gmx.es>
2022-05-20 20:42:36 +0100

    insertando mas ingredientes

--- config/create_database.py
+++ config/create_database.py
@@ -7,11 +7,12 @@ def create_database(db_file):
     conn.execute("CREATE TABLE pizza (idpizza INTEGER PRIMARY KEY, precio INTEGER, nombre text, tamaño text)")
 
     conn.execute("INSERT INTO cliente (nombre,direccion,n_telefono, c_postal) VALUES ('Pepe','Avenida Central',62367784,35014)")
-    conn.execute("INSERT INTO ingredientes VALUES (3450,'queso')")
-    conn.execute("INSERT INTO pedido  VALUES (135)")
     conn.execute("INSERT INTO pizza  VALUES (34, 10, '4 estaciones', 'mediano')")
-    
-
+    conn.execute("INSERT INTO ingredientes VALUES(32424,'tomate')")
+    conn.execute("INSERT INTO ingredientes VALUES (1343,'atun')")
+    conn.execute("INSERT INTO ingredientes VALUES (3450,'queso')")
+    conn.execute("INSERT INTO ingredientes VALUES (3450,'jamon')")
+    conn.execute("INSERT INTO pedido  VALUES (135)")
 
 
     conn.commit()
