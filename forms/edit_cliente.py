b572f1fc0f4c6ecbd25b71e825cd74da9aa50783
Aramis Sarmiento Díaz <aramiissd@gmx.es>
2022-06-11 21:33:32 +0100

    Insercion de datos y visualizacion

--- forms/edit_cliente.py
+++ forms/edit_cliente.py
@@ -1,11 +1,11 @@
-from wtforms import Form, StringField, BooleanField, SubmitField, validators,IntegerField
+from wtforms import Form, StringField,SubmitField, validators,IntegerField
 
 class EditClienteForm(Form):
     
-    telefono = IntegerField('Telefono', [validators.DataRequired()], render_kw={"size" : "50","minlength":"9","maxlength" : "9"})
+    telefono = IntegerField('Telefono', [validators.DataRequired(), validators.NumberRange(min = 600000000,max = 699999999)], render_kw={"size" : "50","minlength":"9","maxlength" : "9"})
     nombre = StringField('Nombre', [validators.DataRequired()], render_kw={"size" : "50", "maxlength" : "20"})
     direccion = StringField ('Dirección', [validators.DataRequired()], render_kw={"size" : "50", "maxlength" : "50"})
-    c_postal = IntegerField ('Código Postal', [validators.DataRequired()], render_kw={"size" : "50", "minlength": "5", "maxlength" : "5"})
+    c_postal = IntegerField ('Código Postal',[validators.DataRequired(), validators.NumberRange(min = 10000,max = 99999)], render_kw={"size" : "50", "minlength": "5", "maxlength" : "5"})
 
     save = SubmitField('Guardar')
     
